51750ea998e9f0bca272be0f5a23a6c4df798995
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2023-08-24 12:42:02 +0600

    table test for mobile final

--- test.js
+++ test.js
@@ -25,4 +25,22 @@ document.getElementById("TableButton").addEventListener("click", function() {
       TableButton.innerText = "View More";
     }
   });
-  
+
+
+var firstTableColumns = document.getElementById("firstTable").rows[0].cells;
+
+var secondTableColumns = document.getElementById("secondTable").rows[0].cells;
+
+for (var i = 0; i < firstTableColumns.length; i++) {
+  secondTableColumns[i].style.width = getComputedStyle(firstTableColumns[i]).width;
+}
+
+var resizeObservers = [];
+for (var i = 0; i < firstTableColumns.length; i++) {
+  resizeObservers[i] = new ResizeObserver(function(entries) {
+    var columnIndex = Array.from(firstTableColumns).indexOf(entries[0].target);
+    secondTableColumns[columnIndex].style.width = getComputedStyle(entries[0].target).width;
+  });
+
+  resizeObservers[i].observe(firstTableColumns[i]);
+}
