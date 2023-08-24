177acb929c994f849b851ed0513ec735d0ab2ee2
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2023-08-24 23:06:54 +0600

    functionality for player ranking

--- test.js
+++ test.js
@@ -1,46 +1,12 @@
-document.getElementById("TableButton").addEventListener("click", function() {
-    var left = document.getElementById("BottomLeftFirst");
-    if (left.classList.contains("table-bottom-left")) {
-      left.classList.remove("table-bottom-left");
-      left.classList.add("table-bottom");
-    } else {
-      left.classList.remove("table-bottom");
-      left.classList.add("table-bottom-left");
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const noStylesDiv = document.querySelector("#no-styles-div");
 
-    var right = document.getElementById("BottomRightFirst");
-    if (right.classList.contains("table-bottom-right")) {
-      right.classList.remove("table-bottom-right");
-      right.classList.add("table-bottom");
-    } else {
-      right.classList.remove("table-bottom");
-      right.classList.add("table-bottom-right");
-    }
-
-    var TableButton = document.getElementById("TableButton");
-    if (TableButton.innerText === "View More") {
-      TableButton.innerText = "View Less";
-    }
-    else {
-      TableButton.innerText = "View More";
-    }
-  });
-
-
-var firstTableColumns = document.getElementById("firstTable").rows[0].cells;
-
-var secondTableColumns = document.getElementById("secondTable").rows[0].cells;
-
-for (var i = 0; i < firstTableColumns.length; i++) {
-  secondTableColumns[i].style.width = getComputedStyle(firstTableColumns[i]).width;
-}
-
-var resizeObservers = [];
-for (var i = 0; i < firstTableColumns.length; i++) {
-  resizeObservers[i] = new ResizeObserver(function(entries) {
-    var columnIndex = Array.from(firstTableColumns).indexOf(entries[0].target);
-    secondTableColumns[columnIndex].style.width = getComputedStyle(entries[0].target).width;
-  });
-
-  resizeObservers[i].observe(firstTableColumns[i]);
-}
+  if (noStylesDiv) {
+    const linkTags = document.querySelectorAll("link[rel='stylesheet']");
+    linkTags.forEach((link) => {
+      if (link.getAttribute("href") === "src/styles.css") {
+        link.remove();
+      }
+    });
+  }
+});
